0dacda43a3e06cc96e66b989c0239dd492a79bc6
xiaoxian <zhshuixian@hotmail.com>
2020-02-06 00:48:35 +0800

    Hello Spring Boot For RESTful API

---
+++ 01-hello-spring-boot/src/test/java/org/xian/boot/BootApplicationTest.java
@@ -0,0 +1,18 @@
+package org.xian.boot;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+class BootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
