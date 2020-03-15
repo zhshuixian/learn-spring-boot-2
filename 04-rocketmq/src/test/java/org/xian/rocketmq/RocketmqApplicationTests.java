5c724999b36a2b0da457a597e6b482e0294f9aca
xiaoxian <zhshuixian@hotmail.com>
2020-03-15 17:30:45 +0800

    Init RocketMQ Project

---
+++ 04-rocketmq/src/test/java/org/xian/rocketmq/RocketmqApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xian.rocketmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
