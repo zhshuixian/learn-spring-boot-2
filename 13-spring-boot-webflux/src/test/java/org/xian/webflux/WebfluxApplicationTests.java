0b0859030d513950e21551b2c7b807e5fff349bc
小先 <zhshuixian@hotmail.com>
2020-05-12 15:39:39 +0800

    webflux

---
+++ 13-spring-boot-webflux/src/test/java/org/xian/webflux/WebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xian.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
